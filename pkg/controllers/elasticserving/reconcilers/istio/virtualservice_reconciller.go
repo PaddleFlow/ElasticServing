ac03588154a5c98bb186d2a6d2ad07fef8ef57b8
jinminhao <jinminhao@sjtu.edu.cn>
2020-12-20 21:24:26 +0800

    Add istio owned by manager

--- pkg/controllers/elasticserving/reconcilers/istio/virtualservice_reconciller.go
+++ pkg/controllers/elasticserving/reconcilers/istio/virtualservice_reconciller.go
@@ -4,6 +4,7 @@ import (
 	"ElasticServing/pkg/controllers/elasticserving/resources/istio"
 
 	core "k8s.io/api/core/v1"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
@@ -26,6 +27,16 @@ func NewVirtualServiceReconciler(client client.Client, scheme *runtime.Scheme, c
 		serviceBuilder: nil,
 	}
 }
+
+// Reconcile reads that state of the cluster for a Profile object and makes changes based on the state read
+// and what is in the Profile.Spec
+// Automatically generate RBAC rules to allow the Controller to read and write Deployments
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=serviceaccount,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=rbac,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=networking.istio.io,resources=virtualservices,verbs=list;create;update;delete
+// +kubebuilder:rbac:groups=elasticserving.paddlepaddle.org,resources=paddlesvcs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=elasticserving.paddlepaddle.org,resources=paddlesvcs/status,verbs=get;update;patch
 
 func (r *VirtualServiceReconciler) Reconcile(paddlesvc *elasticservingv1.PaddleService) error {
 
--- pkg/controllers/elasticserving/resources/istio/virtualservice.go
+++ pkg/controllers/elasticserving/resources/istio/virtualservice.go
@@ -1,5 +1,12 @@
 package istio
 
+import (
+	elasticservingv1 "ElasticServing/pkg/apis/elasticserving/v1"
+
+	"istio.io/client-go/pkg/apis/networking/v1alpha3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
 type IngressConfig struct {
 	IngressGateway     string `json:"ingressGateway,omitempty"`
 	IngressServiceName string `json:"ingressService,omitempty"`
@@ -7,4 +14,21 @@ type IngressConfig struct {
 
 type VirtualServiceBuilder struct {
 	ingressConfig *IngressConfig
+}
+
+func (r *VirtualServiceBuilder) CreateVirtualService(paddlesvc *elasticservingv1.PaddleService) v1alpha3.VirtualService {
+	vs := v1alpha3.VirtualService{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: v1alpha3.SchemeGroupVersion.String(),
+			Kind:       "VirtualService",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        paddlesvc.Name,
+			Namespace:   paddlesvc.Namespace,
+			Labels:      paddlesvc.Labels,
+			Annotations: paddlesvc.Annotations,
+		},
+	}
+
+	return vs
 }
