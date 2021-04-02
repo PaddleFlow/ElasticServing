b8abec3163052fea3d7eac47ff442c88c5c26662
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2021-04-02 10:36:11 +0800

    configmap.go: remove redundant functions
    Signed-off-by: zhangzhengyuan <zhangzhengyuan0604@gmail.com>

--- pkg/apis/elasticserving/v1/configmap.go
+++ pkg/apis/elasticserving/v1/configmap.go
@@ -1,11 +1,7 @@
 package v1
 
 import (
-	"context"
 	"encoding/json"
-
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	core "k8s.io/api/core/v1"
 
@@ -16,17 +12,6 @@ type PaddleServiceConfig struct {
 	ContainerImage string `json:"containerImage"`
 	Version        string `json:"version"`
 	Port           int32  `json:"port"`
-}
-
-func GetPaddleServiceConfig(client client.Client) (*PaddleServiceConfig, error) {
-	configMap := &core.ConfigMap{}
-
-	err := client.Get(context.TODO(), types.NamespacedName{Name: constants.PaddleServiceConfigName, Namespace: constants.PaddleServiceConfigNamespace}, configMap)
-	if err != nil {
-		return nil, err
-	}
-
-	return NewPaddleServiceConfig(configMap)
 }
 
 func NewPaddleServiceConfig(configMap *core.ConfigMap) (*PaddleServiceConfig, error) {
@@ -46,17 +31,6 @@ func NewPaddleServiceConfig(configMap *core.ConfigMap) (*PaddleServiceConfig, er
 type IngressConfig struct {
 	IngressGateway     string `json:"ingressGateway"`
 	IngressServiceName string `json:"ingressServiceName"`
-}
-
-func GetIngresConfig(client client.Client) (*IngressConfig, error) {
-	configMap := &core.ConfigMap{}
-
-	err := client.Get(context.TODO(), types.NamespacedName{Name: constants.PaddleServiceConfigName, Namespace: constants.PaddleServiceConfigNamespace}, configMap)
-	if err != nil {
-		return nil, err
-	}
-
-	return NewIngressConfig(configMap)
 }
 
 func NewIngressConfig(configMap *core.ConfigMap) (*IngressConfig, error) {
