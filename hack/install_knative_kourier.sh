077812c5e0b63f53202a510de8ea83b687f8e7e7
Minhao Jin <minhaoj@andrew.cmu.edu>
2021-07-28 15:11:32 +0800

    update readme

---
+++ hack/install_knative_kourier.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+set -ex
+
+export KNATIVE_VERSION=v0.22.0
+
+# Install knative
+kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-crds.yaml
+kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-core.yaml
+
+# Setup kourier
+kubectl apply --filename https://github.com/knative/net-kourier/releases/download/${KNATIVE_VERSION}/istio.yaml
+kubectl patch configmap/config-network \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
